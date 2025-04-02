cbc8c3a9cb8be8e16dc617e894c7a8bbafa38794
Maroon Ayoub <maroonay@gmail.com>
2025-04-02 18:04:14 +0300

    updated to 2025

--- .devcontainer/fetch-git.sh
+++ .devcontainer/fetch-git.sh
@@ -1,6 +1,6 @@
 git init hw_files
 cd hw_files
-git remote add -f origin https://github.com/OpenUniversity/PL-236319-Spr-2024.git
+git remote add -f origin https://github.com/Technion-Programming-Languages/PL-236319-Spr-2025.git
 git config core.sparseCheckout true 
-echo "Spr24/HW/Homework1/" > .git/info/sparse-checkout 
+echo "Spr25/HW/Homework1/" > .git/info/sparse-checkout 
 git pull origin master
